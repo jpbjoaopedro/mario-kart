308d922646c3d1ee35d03b2c09e2d43a21b791fa
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2024-09-25 23:12:47 -0300

    Adicionando blocos

--- src/index.js
+++ src/index.js
@@ -20,9 +20,33 @@ async function rollDice() {
     return dice;
 }
 
-async function playRaceEngine(character1, character2) {
-    for (let i=0; i<5; i++) {
+async function getRandomBlock() {
+    let random = Math.random();
+    let result;
 
+    switch (true) {
+        case random < 0.33:
+            result = 'RETA';
+            break
+        case random < 0.66:
+            result = 'CURVA';
+            break
+        default:
+            result = 'CONFRONTO';
+    }
+
+    return result;
+}
+
+async function playRaceEngine(character1, character2) {
+    for (let round = 1; round <= 5; round++) {
+        console.log(
+            `🏁 Rodada ${round}`
+        );
+
+        // sortear bloco
+        let block = await getRandomBlock()
+        console.log(`BLOCO: ${block}`)
     }
 }
 
